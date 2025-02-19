74ddcab528a38a059b710c2cfe403b75d47ed995
G1Ismail <ismail.amw@gmail.com>
2025-02-19 08:53:52 +0100

    Project Struktur, Setup und  Dependencies festgelegt und installiert

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medecin Gen AI Project',
+    version = '0.0.0',
+    author = 'Ismail Ghosn',
+    author_email = 'ismail.amb7@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+)
